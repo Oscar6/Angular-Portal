86bce6cd9261fab3832346d6fa5ae1c2f8640fad
Oscar6 <oscar.m1024@gmail.com>
2019-09-06 17:40:24 -0500

    fix firebase function issue

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+const functions = require('firebase-functions');
+
+exports.addToFollowing = functions.database.ref('/follow/{initiatorUid}/{interestedInFollowingUid}')
+    .onCreate(event => {
+        const initiatorUid = event.params.initiatorUid;
+        const interestedInFollowingUid = event.params.interestedInFollowingUid;
+        const rootRef = event.data.ref.root;
+        let FollowingMeRef = rootRef.child('usersFollowingMe/' + interestedInFollowingUid + "/" + initiatorUid);
+        return FollowingMeRef.set(true);
+    });
+
+    // Continue firebase functions tutorial
